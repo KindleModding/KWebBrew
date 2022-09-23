47961019aa2ba955e2d0624a1b677c3df72d9e77
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-09-23 17:02:43 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,29 @@
+function fetchData(url, ) {
+  // Create iframe
+  var iframe = document.createElement("iframe");
+  iframe.src = url;
+
+  // Add iframe to body
+  document.body.appendChild(iframe);
+
+  // Add listener to iframe
+  iframe.addEventListener("load", function (event) {
+    // Get iframe's documentElement and clone it and get the innerHTML
+    iframeSource = (iframe.contentDocument.documentElement || iframe.contentWindow.document.documentElement).cloneNode(true).innerHTML;
+
+    // Remove iframe from DOM
+    document.body.removeChild(iframe);
+
+    // Callback function
+    resolveresolve(iframeSource);
+  });
+}
+
+// Button function to get source of file listings and such
+function getSource() {
+  // Get the source displaying element
+  var sourcer = document.getElementById("sourcesource");
+
+  // Fetch data with source callback
+  fetchData(document.getElementById("textInput").value, function (text) { sourcer.innerText = text; });
+}
