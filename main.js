41b7a3c84a521e492d1298a704cc19cf6e47b3f6
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-09-24 10:28:19 +0100

    Migrate directory code to function

--- main.js
+++ main.js
@@ -26,6 +26,32 @@ function fetchData(url) {
 }
 
 
+function getDirectory(location) {
+  return new Promise(function (callback) {
+    fetchData(location).then(function (data) {
+      var files = [];
+
+      const tagReg = new RegExp('<a(\n|.)*?(?=<\/a>)', "gim");
+      const hrefReg = new RegExp('href="(\n|.)*?(?=")', 'gim');
+      const tagRemovalReg = new RegExp('<a(\n|.)*?>', 'gim');
+
+      const anchorTags = Array.from(data.matchAll(tagReg));
+
+      for (var i=0; i < anchorTags.length; i++) {
+        const source = Array.from(anchorTags[i][0].matchAll(hrefReg));
+
+        files.push({
+          "name": anchorTags[i][0].replace(tagRemovalReg, '').toString(),
+          "path": source[0][0].substring(6).toString()
+        });
+      }
+
+      callback(files);
+    });
+  });
+}
+
+
 // Button function to get source of file listings and such
 function getSource() {
   // Get the source displaying element
